037605f6305d7ab31ae5c16391f17336b353146e
Gabriel Low <lowgabriel@ymail.com>
2022-05-31 03:59:05 +0800

    removed BibleManager

--- App.js
+++ App.js
@@ -1,3 +1,19 @@
 class App {
-    
+
+    #totalBible = 1;
+    #totalBibleLimit = 4;
+
+    constructor() {
+
+    }
+
+    _loadGrid() {
+
+    }
+
+    _addBible() {
+        if (this.#totalBible < this.#totalBibleLimit) {
+            this.#totalBible += 1;
+        }
+    }
 }
