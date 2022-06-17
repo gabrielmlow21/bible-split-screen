78d1f0ae53bd5fb43b83e7ae83546fa4b64792ff
Gabriel Low <lowgabriel@ymail.com>
2022-06-17 23:07:10 +0800

    refactored codebase

--- contentScript.js
+++ contentScript.js
@@ -1,5 +1,5 @@
-let verses = [["", "", ""], ["", "", ""]];
-
+let verses = [];
+let totalVerseLimit = 2;
 const gridEl = document.createElement("DIV");
 gridEl.classList.add('grid');
 gridEl.classList.add('grid-2'); // FORCED
@@ -30,71 +30,47 @@ document.body.insertBefore(gridEl, document.body.firstChild);
 // footer[0].appendChild(bible2El);
 
 window.setInterval(function() {
-
     const currentVerse = document.getElementById("pVerse").innerText;
     const currentVersion = document.getElementById("pTranslation").innerText;
-
-    if (verses.length === 0 || verses[0][0] !== currentVerse || verses[0][1] !== currentVersion) {
-
-        const content = document.getElementById("pContent").innerText != null ? document.getElementById("pContent").innerText : "no content";  
-        const verse = currentVerse;
-        const version = currentVersion;
-
-        verses.unshift([verse, version, content]);
-        
-        const firstVerseText = document.createTextNode(verses[0][0]);
-        const firstVersionText = document.createTextNode(verses[0][1]);
-        const firstContentText = document.createTextNode(verses[0][2]);
-        const secondVerseText = document.createTextNode(verses[1][0]);
-        const secondVersionText = document.createTextNode(verses[1][1]);
-        const secondContentText = document.createTextNode(verses[1][2]);
-
-        const firstBibleContainerEl = document.createElement('div');
-        const firstVerseEl = document.createElement('div');
-        const firstContentEl = document.createElement('div');
-        const firstVersionEl = document.createElement('div');
-        const secondBibleContainerEl = document.createElement('div');
-        const secondVerseEl = document.createElement('div');
-        const secondContentEl = document.createElement('div');
-        const secondVersionEl = document.createElement('div');
-    
-        firstBibleContainerEl.classList.add('bible-container', 'bible-container-2');
-        firstVerseEl.classList.add('verse', 'verse-2');
-        firstContentEl.classList.add('content', 'content-2');
-        firstVersionEl.classList.add('version', 'version-2');
-        secondBibleContainerEl.classList.add('bible-container', 'bible-container-2');
-        secondVerseEl.classList.add('verse', 'verse-2');
-        secondContentEl.classList.add('content', 'content-2');
-        secondVersionEl.classList.add('version', 'version-2');
-    
-        firstVerseEl.appendChild(firstVerseText);
-        firstContentEl.appendChild(firstContentText);
-        firstVersionEl.appendChild(firstVersionText);
-        secondVerseEl.appendChild(secondVerseText);
-        secondContentEl.appendChild(secondContentText);
-        secondVersionEl.appendChild(secondVersionText);
-
-        firstBibleContainerEl.appendChild(firstVerseEl);
-        firstBibleContainerEl.appendChild(firstVersionEl);
-        firstBibleContainerEl.appendChild(firstContentEl);
-        secondBibleContainerEl.appendChild(secondVerseEl);
-        secondBibleContainerEl.appendChild(secondVersionEl);
-        secondBibleContainerEl.appendChild(secondContentEl);
-
-        gridEl.textContent = '';
-        gridEl.appendChild(firstBibleContainerEl);  
-        gridEl.appendChild(secondBibleContainerEl);  
-
-        // replaceOptions();
+    const currentContent = document.getElementById("pContent").innerText != null ? document.getElementById("pContent").innerText : "no content";  
+    if (verses.length === 0 || verses[0].verse !== currentVerse || verses[0].version !== currentVersion) {
+        const bible = {
+            verse: currentVerse,
+            version: currentVersion,
+            content: currentContent
+        }
+        verses.unshift(bible)
+        console.log(verses);
+        renderVerses();
     }
-    // console.log(verses)
 }, 1000);
 
 
-// function replaceOptions() {
-//     const newBible1Select = document.createElement("SELECT");
-//     bible1Select.classList.add('bible1-select');
-//     bible1Select.setAttribute("name", "bible1");
-//     bible1Select.classList.add("bible1Select");
-//     console.log(verses)
-// }
+function renderVerses() {
+    gridEl.textContent = '';    // reset content at grid
+    for (let i = 0; i < totalVerseLimit; i++) {
+        // stop rendering if it reaches the verses length
+        if (i == verses.length) {
+            return;
+        }
+        const bibleContainerEl = document.createElement('div');
+        const verseEl = document.createElement('div');
+        const contentEl = document.createElement('div');
+        const versionEl = document.createElement('div');
+
+        bibleContainerEl.classList.add('bible-container', 'bible-container-2');
+        verseEl.classList.add('verse', 'verse-2');
+        contentEl.classList.add('content', 'content-2');
+        versionEl.classList.add('version', 'version-2');
+
+        verseEl.appendChild(document.createTextNode(verses[i].verse));
+        versionEl.appendChild(document.createTextNode(verses[i].version));
+        contentEl.appendChild(document.createTextNode(verses[i].content));
+
+        bibleContainerEl.appendChild(verseEl);
+        bibleContainerEl.appendChild(versionEl);
+        bibleContainerEl.appendChild(contentEl);
+
+        gridEl.appendChild(bibleContainerEl);  
+    }
+}
