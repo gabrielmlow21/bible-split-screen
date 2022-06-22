23a98f7c24c2bb54acdbf5b9aa71ff964dc03070
Gabriel Low <lowgabriel@ymail.com>
2022-06-22 16:53:59 +0800

    added string literal

--- popup.js
+++ popup.js
@@ -3,7 +3,7 @@ setInterval(updateDropdownOptions, 1000);
 function updateDropdownOptions() {
     chrome.storage.sync.get(["verses", "selectedVerses", "totalBibleOnScreen"], (res) => {
         for (let bibleId = 0; bibleId < res.totalBibleOnScreen; bibleId++) {
-            let str = (bibleId == res.selectedVerses[bibleId]) ? "<option value=" + "default" + " selected>Default</option>" : "<option value=" + "default" + ">Default</option>";
+            let str = (bibleId == res.selectedVerses[bibleId]) ? `<option value="default" selected>Default</option>` : `<option value="default">Default</option>`;
             for (let i = 0; i < res.verses.length; i++) {
                 str += (i == res.selectedVerses[bibleId]) ? `<option value="${i}" selected>${res.verses[i].verse} (${res.verses[i].version})</option>` : `<option value="${i}">${res.verses[i].verse} (${res.verses[i].version})</option>`; 
             }
