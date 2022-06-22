27d61e10ac7ee522676959556c3ec8ad98c36b53
Gabriel Low <lowgabriel@ymail.com>
2022-06-22 16:51:42 +0800

    added feature that selects selected verses from dropdown

--- popup.js
+++ popup.js
@@ -1,19 +1,13 @@
 setInterval(updateDropdownOptions, 1000);
 
 function updateDropdownOptions() {
-    chrome.storage.sync.get(["verses"], (res) => {
-        str = "<option value=" + "default" + ">Default</option>";
-        for (let i = 0; i < res.verses.length; i++) {
-            str += "<option value=" + i + ">" + res.verses[i].verse + " (" + res.verses[i].version + ") </option>";
+    chrome.storage.sync.get(["verses", "selectedVerses", "totalBibleOnScreen"], (res) => {
+        for (let bibleId = 0; bibleId < res.totalBibleOnScreen; bibleId++) {
+            let str = (bibleId == res.selectedVerses[bibleId]) ? "<option value=" + "default" + " selected>Default</option>" : "<option value=" + "default" + ">Default</option>";
+            for (let i = 0; i < res.verses.length; i++) {
+                str += (i == res.selectedVerses[bibleId]) ? `<option value="${i}" selected>${res.verses[i].verse} (${res.verses[i].version})</option>` : `<option value="${i}">${res.verses[i].verse} (${res.verses[i].version})</option>`; 
+            }
+            document.getElementById(`bible-${bibleId+1}`).innerHTML = str;
         }
-        document.getElementById("bible-1").innerHTML = str;
-        document.getElementById("bible-2").innerHTML = str;
-        document.getElementById("bible-3").innerHTML = str;
-        document.getElementById("bible-4").innerHTML = str;
     })
-}
-
-
-function updateselectedVerses(value) {
-    
 }
